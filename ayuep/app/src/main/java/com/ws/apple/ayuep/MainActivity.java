6c3cd04a9cecb3e10e940661d5db13e5ffcf9fb3
Keven <527030163@qq.com>
2016-07-05 22:14:08 +0800

    Add store info view.

--- ayuep/app/src/main/java/com/ws/apple/ayuep/MainActivity.java
+++ ayuep/app/src/main/java/com/ws/apple/ayuep/MainActivity.java
@@ -1,15 +1,19 @@
 package com.ws.apple.ayuep;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListActivity listActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    protected 
+        String[] types = new String[]{"111", "222", "333"};
+
+    }
 }
